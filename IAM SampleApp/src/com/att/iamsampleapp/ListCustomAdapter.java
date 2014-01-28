4a0f0816dc9444a658cc83228babba8324ae7163
Dinesh Kumar <dg185p@att.com>
2014-01-27 21:17:46 -0800

    Message List issue Fix

--- IAM SampleApp/src/com/att/iamsampleapp/ListCustomAdapter.java
+++ IAM SampleApp/src/com/att/iamsampleapp/ListCustomAdapter.java
@@ -14,20 +14,16 @@ public class ListCustomAdapter extends BaseAdapter {
 
 	private LayoutInflater mInflater;
 
-	//public ListCustomAdapter(Context context, MessageList results) {
 	public ListCustomAdapter(Context context, Message[] results) {
-		//messageList = results;
 		messageList = results;
 		mInflater = LayoutInflater.from(context);
 	}
 
 	public int getCount() {
-		//return messageList.getLimit();
 		return messageList.length;
 	}
 
 	public Object getItem(int position) {
-		//return messageList.getMessages()[position];
 		return messageList[position];
 	}
 
@@ -38,30 +34,25 @@ public class ListCustomAdapter extends BaseAdapter {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		ViewHolder holder;
 		
-		//if (convertView == null && messageList.getTotal() < position) {
 		if (convertView == null) {
+			
 			convertView = mInflater.inflate(R.layout.custom_row_view, null);
+			
 			holder = new ViewHolder();
-			
 			holder.txtName = (TextView) convertView.findViewById(R.id.name);
-			//holder.txtName.setText(messageList.getMessages()[position].getFrom());
-			holder.txtName.setText(messageList[position].getFrom());
-			
 			holder.txtMessage = (TextView) convertView
 					.findViewById(R.id.message);
-			//holder.txtMessage.setText(messageList.getMessages()[position].getText());
-			holder.txtMessage.setText(messageList[position].getText());
-			
 			holder.txtTime = (TextView) convertView.findViewById(R.id.time);
-			//holder.txtTime.setText(messageList.getMessages()[position].getTimeStamp());
-			holder.txtTime.setText(messageList[position].getTimeStamp());
 
 			convertView.setTag(holder);
 		} else {
-			
 			holder = (ViewHolder) convertView.getTag();
 		}
-
+		
+		holder.txtName.setText(messageList[position].getFrom());
+		holder.txtMessage.setText(messageList[position].getText());
+		holder.txtTime.setText(messageList[position].getTimeStamp());
+		
 		return convertView;
 	}
 
