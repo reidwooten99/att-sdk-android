a13743d1850f8a004ee83f3d500b88b5e30d80df
Surender Mohan <smohan36@gmail.com>
2014-04-16 13:03:11 -0700

    refactored the code to do the cleanup and remove test code

--- IAMSDK/src/com/att/api/aab/service/AsyncApiWrapper.java
+++ IAMSDK/src/com/att/api/aab/service/AsyncApiWrapper.java
@@ -14,141 +14,56 @@ import com.att.api.error.Utils;
 import com.att.api.immn.listener.ATTIAMListener;
 import com.att.api.rest.RESTException;
 
-public class AsyncApiWrapper implements ATTIAMListener {
+public class AsyncApiWrapper {
 	
 	private ATTIAMListener iamListener;
-	//private GetContactParams contactParams;
-	//private String xFields;
-	AABService aabSrvc;
-	protected Handler handler = new Handler();
+	private AABService aabSrvc;
 	
 	public AsyncApiWrapper(AABService aabService, ATTIAMListener iamListener) {
-
-		//this.xFields = xFields;
-		//this.contactParams = new GetContactParams(xFields, pageParams, searchParams);		
 		this.iamListener = iamListener;
 		this.aabSrvc = aabService;
 	}
-	
 	
 	public void GetContacts(GetContactParams contactParams) {
 		GetContactsTask getContactsTask = new GetContactsTask();
 		getContactsTask.execute(contactParams);
 	}
 	
-	public class  GetContactsTask extends AsyncTask<GetContactParams, Void, String> {
-
+	public class  GetContactsTask extends AsyncTask<GetContactParams, Void, ContactResultSet> {
 		@Override
-		//protected ContactResultSet doInBackground(GetContactParams... params) {
-		protected String doInBackground(GetContactParams... params) {
-			// TODO Auto-generated method stub
-			//ContactResultSet contactResultSet = null;
-			String strResult = null;
+		protected ContactResultSet doInBackground(GetContactParams... params) {
+			ContactResultSet contactResultSet = null;
 			InAppMessagingError errorObj = new InAppMessagingError();
 
 			try {
-				//contactResultSet = aabSrvc.getContacts(
-				strResult = aabSrvc.getContactsResponse(
+				contactResultSet = aabSrvc.getContacts(
 								params[0].getxFields(), //xFields
-													   params[0].getPageParams(), //PageParams
-													   params[0].getSearchParams() //SearchParams 
-													   );
+							    params[0].getPageParams(), //PageParams
+							    params[0].getSearchParams() //SearchParams 
+							    );
 			} catch (RESTException e) {
 				errorObj = Utils.CreateErrorObjectFromException( e );
-				onError( errorObj );
+				if (null != iamListener) {
+					iamListener.onError(errorObj);
+				}
 			} catch (ParseException e) {
 				errorObj = new InAppMessagingError(e.getMessage());
-				onError(errorObj);		
+				if (null != iamListener) {
+					iamListener.onError(errorObj);
+				}
 			}
 			
-			return strResult; // contactResultSet;
+			return contactResultSet;
 		}
 
 		@Override
-		protected void onPostExecute(String contactResultSet) {
-			// TODO Auto-generated method stub
+		protected void onPostExecute(ContactResultSet contactResultSet) {
 			super.onPostExecute(contactResultSet);
 			if( null != contactResultSet ) {
-				// onSuccess(contactResultSet); // SM Note: This is not working.
 				if (null != iamListener) {
-			        JSONObject jrs = null;
-					try {
-						jrs = new JSONObject(contactResultSet).getJSONObject("resultSet");
-					} catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					iamListener.onSuccess(ContactResultSet.valueOf(jrs));
+					iamListener.onSuccess(contactResultSet);
 				}
-			}
-			
-		}
-		
+			}			
+		}		
 	}
-
-	@Override
-	public void onSuccess(final Object contactResultSet) {
-		// TODO Auto-generated method stub
-		handler.post(new Runnable() {
-
-			@Override
-			public void run() {
-				if (null != iamListener) {
-			        JSONObject jrs = null;
-					try {
-						jrs = new JSONObject(contactResultSet).getJSONObject("resultSet");
-					} catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					iamListener.onSuccess(ContactResultSet.valueOf(jrs));
-				}
-			}
-			
-		});
-		
-	}
-
-	@Override
-	public void onError(final InAppMessagingError error) {
-/*		String serviceExceptionId = null;
-		
-		if(null  != error) {
-			if(error.getHttpResponse().contains("ServiceException")) {
-				JSONObject jobj;
-				MessageIndexInfo messageIndexInfo;
-				try {
-					jobj = new JSONObject( error.getHttpResponse());
-					JSONObject jobj1 = jobj.getJSONObject("RequestError");
-					JSONObject jobj2 = jobj1.getJSONObject("ServiceException");
-					serviceExceptionId = jobj2.getString("MessageId");
-					if (serviceExceptionId.equalsIgnoreCase("SVC0001") ) {
-						messageIndexInfo = immnSrvc.getMessageIndexInfo();
-						if(messageIndexInfo.getState().equalsIgnoreCase("NOT_INITIALIZED") || 
-						   messageIndexInfo.getState().equalsIgnoreCase("ERROR")) {
-							immnSrvc.createMessageIndex();
-							GetMessageList();
-						}
-					}
-				} catch (JSONException e1) {
-					e1.printStackTrace();
-				} catch (RESTException e) {
-					e.printStackTrace();
-				}  catch (ParseException e) {
-					e.printStackTrace();
-				}
-			}
-		}	*/
-					
-		handler.post(new Runnable() {
-			@Override
-			public void run() {
-				if (null != iamListener) {
-					iamListener.onError(error);
-				}
-			}
-		});
-		
-	}
-
 }
