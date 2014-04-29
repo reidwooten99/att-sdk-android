a91d4b06edc191eef7979f526be6acc0cec2b225
Surender Mohan <smohan36@gmail.com>
2014-04-29 16:16:01 -0700

    add prod keys also

--- TestAABApp/src/com/att/testaab/Config.java
+++ TestAABApp/src/com/att/testaab/Config.java
@@ -11,9 +11,10 @@ public class Config {
 	public static final int messageOffset 			= 	0;
 	public static final int maxRecipients 			= 	10;
 	public static final int	maxAttachments			= 	21;
-	public static final String fqdn		 			= 	"https://api-stage.mars.bf.sl.attcompute.com";
+	public static final String f6fqdn		 		= 	"https://api-stage.mars.bf.sl.attcompute.com";
 	public static final String ldevFqdn				=	"http://ldev.code-api-att.com:8888";
-	public static final String iamFqdn				= 	"https://api.att.com";
+	public static final String prodFqdn				= 	"https://api.att.com";
+	public static final String fqdn				= 	"https://api.att.com";
 	
 	//public static final String f6Fqdn		 		= 	"https://api-stage.mars.bf.sl.attcompute.com";
 	//public static String f6token 					= 	"ERVYgpBqYbFJMnWvCAnhqJVOUXShDOQD";
@@ -24,9 +25,12 @@ public class Config {
 	public static final String appScope	 			= 	 APP_SCOPE;
 	public static final String redirectUri  		= 	 REDIRECT_URI;*/
 	
-	public static final String clientID				= 	 "qyptay8tyizxqvp2teeiipqeofetdogz";//"7vroavot7vittuzg8zegqszjnymyf3lw";
-	public static final String secretKey 			= 	 "fi7vwipukqnlhgxpjchi1kmwbiwa4kwj";//"tvygie2blq8gf1yhshylf9kqspi7gfvx";
-	public static final String appScope	 			= 	 "AAB";
-	public static final String redirectUri  		= 	"https://www.something.com";
+	//public static final String clientID			= 	 "qyptay8tyizxqvp2teeiipqeofetdogz";
+	//public static final String secretKey 			= 	 "fi7vwipukqnlhgxpjchi1kmwbiwa4kwj";
+	//public static final String redirectUri  		= 	"https://www.something.com";
+	public static final String clientID				= 	 "7vroavot7vittuzg8zegqszjnymyf3lw";
+	public static final String secretKey 			= 	 "tvygie2blq8gf1yhshylf9kqspi7gfvx";
+	public static final String redirectUri  		= 	"https://developer.att.com";
+	public static final String appScope	 			= 	 "IMMN,MIM";
 	public static final String iamDownloadDirectory = "InAppMessagingDownloads";
 }
