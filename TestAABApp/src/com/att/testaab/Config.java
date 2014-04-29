2ec69cb8c55ca30d220a5f1b091aea8f651cfdbf
poojawork <pooja.work@outlook.com>
2014-04-29 11:10:21 -0700

    Added F6 Environment credentials

--- TestAABApp/src/com/att/testaab/Config.java
+++ TestAABApp/src/com/att/testaab/Config.java
@@ -1,27 +1,25 @@
 package com.att.testaab;
 
-import com.att.api.oauth.OAuthToken;
 
 public class Config {	
-	public static String token = "abcd";
+	public static String token;
 	public static long accessTokenExpiry = 50000000;
-	public static String refreshToken = "wxyz";
+	public static String refreshToken ;
 	public static final int messageLimit 			= 	500;
 	public static final int messageOffset 			= 	0;
 	public static final int maxRecipients 			= 	10;
 	public static final int	maxAttachments			= 	21;
-	public static final String fqdn		 			= 	"http://ldev.code-api-att.com:8888";
+	public static final String fqdn		 			= 	"https://api-stage.mars.bf.sl.attcompute.com";
 	public static final String iamFqdn				= 	"https://api.att.com";
-	//public static OAuthToken authToken 				= 	new OAuthToken("abcd", 1, "xyz");
 	
 	/*public static final String clientID			= 	 APP_KEY;
 	public static final String secretKey 			= 	 APP_SECRET;
 	public static final String appScope	 			= 	 APP_SCOPE;
 	public static final String redirectUri  		= 	 REDIRECT_URI;*/
 	
-	public static final String clientID				= 	 "7vroavot7vittuzg8zegqszjnymyf3lw";
-	public static final String secretKey 			= 	 "tvygie2blq8gf1yhshylf9kqspi7gfvx";
-	public static final String appScope	 			= 	 "IMMN,MIM";
-	public static final String redirectUri  		= 	 "https://developer.att.com";
+	public static final String clientID				= 	 "qyptay8tyizxqvp2teeiipqeofetdogz";//"7vroavot7vittuzg8zegqszjnymyf3lw";
+	public static final String secretKey 			= 	 "fi7vwipukqnlhgxpjchi1kmwbiwa4kwj";//"tvygie2blq8gf1yhshylf9kqspi7gfvx";
+	public static final String appScope	 			= 	 "AAB";
+	public static final String redirectUri  		= 	"https://www.something.com";
 	public static final String iamDownloadDirectory = "InAppMessagingDownloads";
 }
