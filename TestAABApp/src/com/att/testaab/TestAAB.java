824c1df12049d54f0d2e1e7490bf92a387e9d8d3
poojawork <pooja.work@outlook.com>
2014-05-13 18:12:58 -0700

    cleanup - the switch case block

--- TestAABApp/src/com/att/testaab/TestAAB.java
+++ TestAABApp/src/com/att/testaab/TestAAB.java
@@ -95,328 +95,92 @@ public class TestAAB extends Activity implements OnClickListener {
 			@Override
 			public void onClick(View v) {
 				ContactsTestCase ctc = new ContactsTestCase(displayContacts, null);
+				GroupsTestCase gtc = new GroupsTestCase(displayContacts, null);
+				OtherTestCase otc  = new OtherTestCase(displayContacts, null);
 				
 			    String apiNumber = testApi.getText().toString();
                 int iOperation = 1;
                 try { iOperation = Integer.valueOf(apiNumber); }
                 catch(Exception e) {}
                 
-               // OAuthToken authToken = new OAuthToken(Config.token, Config.accessTokenExpiry, Config.refreshToken);
 				switch (iOperation) {
 				
 					case 1: //GetContacts
-						ctc.testGetContacts("shallow", pageParams, searchParams);
-						break;
+							ctc.testGetContacts("shallow", pageParams, searchParams);
+							break;
 						
 					case 2: //GetContact
-						ctc.testGetContact("0987654432123", "shallow");	
-						break;
+							ctc.testGetContact("12CE1C28362013082800371294D457585C50A2B1", "shallow");	
+							break;
 						
 					case 3: //GetContactGroups
-						aabManager = new AABManager(Config.fqdn, 
-													authToken,
-													new getContactGroupsListener());
-						//aabManager.GetContact("09876544321", "shallow");
-						pageParams = new PageParams("ASC", "firstName", "2", "0");
-						aabManager.GetContactGroups("0987654432123", pageParams);	
-						break;
+							pageParams = new PageParams("ASC", "groupName", "12", "0");
+							ctc.testGetContactGroups("12CE1C28362013082800371294D457585C50A2B1", pageParams);
+							break;
 						
 					case 4: //CreateContact
-						ctc.testCreateContact("First", "Last");
-						break;
+							ctc.testCreateContact("TestFirstFive", "Last");
+							break;
 						
-					case 5: //UpdateContact //getcontacts n then update
-						aabManager = new AABManager(Config.fqdn,
-													authToken,
-													new getContactsforUpdateListener());
-						aabManager.GetContacts("shallow", pageParams, searchParams);
-						/*Contact.Builder builderForUpdate = new Contact.Builder(); 
-						builderForUpdate.setFirstName("Last");
-						builderForUpdate.setLastName("First");
-						builderForUpdate.setFormattedName("LastFirst");
-						
-						Contact contactForupdate = builderForUpdate.build();
-						aabManager.UpdateContact(contactForupdate);		*/			
-						break; 
+					case 5: //UpdateContact 
+							/*aabManager = new AABManager(Config.fqdn,
+														authToken,
+														new getContactsforUpdateListener());
+							aabManager.GetContacts("shallow", pageParams, searchParams);
+							Contact.Builder builderForUpdate = new Contact.Builder(); 
+							builderForUpdate.setFirstName("Last");
+							builderForUpdate.setLastName("First");
+							builderForUpdate.setFormattedName("LastFirst");
+							
+							Contact contactForupdate = builderForUpdate.build();
+							aabManager.UpdateContact(contactForupdate);		*/			
+							break; 
 					
 					case 6: //DeleteContact
-						ctc.testDeleteContact(ctc.newContactId);
-						break;
+							ctc.testDeleteContact(ctc.newContactId);
+							break;
 						
 					case 7 : //GetGroups
-						aabManager = new AABManager(Config.fqdn,
-													authToken,
-													new getGroupsListener());
-						aabManager.GetGroups(pageParams, null);
-						break;
-						
-					case 8:  //CreateGroup  - creates, gets, updates and shows(gets) the groups
-						aabManager = new AABManager(Config.fqdn,
-													authToken,
-													new createGroupListener());
-						Group newGroup = new Group("0505","TestGroup","USER");
-						aabManager.CreateGroup(newGroup);
-						break;
+							pageParams = new PageParams("ASC", "groupName", "2", "0");
+							gtc.testGetGroups( pageParams, null);
+							break;
+							
+					case 8:  //CreateGroup 
+							Group newGroup = new Group("05058","TestGroup8","USER");
+							gtc.testCreateGroup(newGroup);
+							break;
 						
 					case 9: //DeleteGroup
-						aabManager = new AABManager(Config.fqdn,
-													authToken,
-													new deleteGroupListener());
-						aabManager.DeleteGroup(groupSubscriberId);		
-						break;
+							gtc.testDeleteGroup("39597cb2-a609-4696-96c7-71d09f09907d");
+							break;
 						
 					case 10: //UpdateGroup
-						break;
+							break;
 						
 					case 11: //AddContactsToGroup
+							gtc.testAddContactsToGroup("a749f6b6-a563-42e9-89c3-0cda468a043e" , "39FC506FE22013082800371294D457585C50A2B1");
 							break;
 					
 					case 12: //RemoveContactsFromGroup
+							gtc.testRemoveContactsFromGroup("a749f6b6-a563-42e9-89c3-0cda468a043e", "12CE1C28362013082800371294D457585C50A2B1");
 							break;
 					
 					case 13: //GetGroupContacts
+							pageParams = new PageParams("ASC", "firstName", "10", "0");
+							gtc.testGetGroupContacts("a749f6b6-a563-42e9-89c3-0cda468a043e", pageParams);
 							break;
 							
 					case 14: //GetMyInfo
+							otc.testGetMyInfo();
 							break;
 					
 					case 15: //UpdateMyInfo
-							break;
-					
-						
+							//otc.testUpdateMyInfo(contact);
+							break;					
 				}
 			}
 		});
 		
-	}
-
-	private class getContactGroupsListener extends UnitTestListener {
-
-		public getContactGroupsListener() {
-			super("GetContactGroups", displayContacts, null);
-		}
-
-		@Override
-		public void onSuccess(Object response) {
-			GroupResultSet groupResultSet = (GroupResultSet) response;
-			if (null != groupResultSet) {
-				strText = "\nPassed: " + strTestName + " test.";
-				Group[] groups_arr = groupResultSet.getGroups();
-				for (int i=0; i < groups_arr.length; i++) {
-					Group grp = groups_arr[i];
-					strText += "\n" + grp.getGroupId() + ", " + grp.getGroupName() + ", "  + grp.getGroupType();
-				}
-			} else {
-				strText = "Unknown: " + strTestName + " test.\nNo data returned.";				
-			}
-			updateTextDisplay(strText);
-			return;
-		}
-	}	
-	
-	private class getContactsforUpdateListener implements ATTIAMListener {
-
-		@Override
-		public void onSuccess(Object response) {
-			ContactResultSet contactResultSet = (ContactResultSet) response;
-			
-			if (null != contactResultSet) {
-				strText = (String) displayContacts.getText();
-				//QuickContact[] quickContactsArray = contactResultSet.getQuickContacts();
-				Contact[] contactsArray = contactResultSet.getContacts();
-				
-				Contact.Builder builderForUpdate = new Contact.Builder(); 
-				builderForUpdate.setContactId(contactsArray[0].getContactId());
-				builderForUpdate.setFirstName("Last");
-				builderForUpdate.setLastName("First");
-				builderForUpdate.setFormattedName("LastFirst");
-				Contact contactForUpdate  = builderForUpdate.build();
-				
-				aabManager = new AABManager(Config.fqdn,
-											authToken,
-											new updateContactListener());
-				aabManager.UpdateContact(contactForUpdate);
-				
-				Log.i("updateContactAPI","OnSuccess : ContactID :  " + strText);
-				//Log.i("getContactsAPI", "OnSuccess : ContactID :  " +contactResultSet.getQuickContacts()[1].getContactId().toString());
-				displayContacts.setText(strText);
-				return;
-			}
-			
-		}
-
-		@Override
-		public void onError(InAppMessagingError error) {
-			Log.i("updateContactAPI on error", "onError");
-
-		}
-	}
-	
-	private class updateContactListener implements ATTIAMListener {
-
-		@Override
-		public void onSuccess(Object response) {
-			String result = (String) response;
-			strText = (String) displayContacts.getText();
-			strText += "\n" +"updateContactAPI : " + "\n" + result;
-			Log.i("updateContactAPI","OnSuccess : RESULT :  " + result);
-			displayContacts.setText(strText);
-			return;
-			
-		}
-
-		@Override
-		public void onError(InAppMessagingError error) {
-			Log.i("updateContactAPI on error", "onError");
-
-		}
-	}
-	
-	private class getGroupsListener implements ATTIAMListener {
-
-		@Override
-		public void onSuccess(Object response) {
-			GroupResultSet groupResultSet = (GroupResultSet) response;
-			
-			if (null != groupResultSet) {
-				strText = (String) displayContacts.getText();
-				Group[] groups_arr = groupResultSet.getGroups();
-				for (int i=0; i < groups_arr.length; i++) {
-					Group grp = groups_arr[i];
-					strText += "\n" + grp.getGroupId() + ", " + 
-							grp.getGroupName() + ", " + grp.getGroupType();
-					
-				}
-			Log.i("getGroupsAPI","OnSuccess : RESULT :  " + strText);
-			displayContacts.setText(strText);
-			
-			String grpName ="UpdateGroup";
-			Group udpateGroup = new Group("0505",grpName,null);
-
-			aabManager = new AABManager(Config.fqdn,
-										authToken,
-										new updateGroupListener());
-			aabManager.UpdateGroup(udpateGroup);
-			
-			return;
-			}	
-		}
-
-		@Override
-		public void onError(InAppMessagingError error) {
-			Log.i("getGroupsAPI on error", "onError");
-
-		}
-	}
-	
-	private class createGroupListener implements ATTIAMListener {
-
-		@Override
-		public void onSuccess(Object response) {
-			String location = (String) response;
-			if (null != location) {
-				strText = (String) displayContacts.getText();
-				Log.i("createGroupAPI","Group created : Location :  " + location);
-				strText += "\n" + location;
-				
-				String[] locationUrl = location.split("groups/");
-				groupSubscriberId = locationUrl[1];
-				Log.i("createGroupAPI","Group created : groupId :  " + groupSubscriberId);
-				
-				aabManager = new AABManager(Config.fqdn,
-											authToken,
-											new getGroupsListener());
-				aabManager.GetGroups(pageParams, "TestGroup");
-								
-				displayContacts.setText(strText);
-				return;
-			}	
-		}
-
-		@Override
-		public void onError(InAppMessagingError error) {
-			Log.i("createGroupAPI on error", "onError");
-
-		}
-	}
-	
-	private class deleteGroupListener implements ATTIAMListener {
-
-		@Override
-		public void onSuccess(Object response) {
-			String result = (String) response;
-			strText = (String) displayContacts.getText();
-			strText += "\n" +"deleteGroupAPI : " + "\n" + result;
-			Log.i("deleteGroupAPI","OnSuccess : RESULT :  " + result);
-			displayContacts.setText(strText);
-			return;
-			
-		}
-
-		@Override
-		public void onError(InAppMessagingError error) {
-			Log.i("deleteGroupAPI on error", "onError");
-
-		}
-	}
-	
-	private class updateGroupListener implements ATTIAMListener {
-
-		@Override
-		public void onSuccess(Object response) {
-			String result = (String) response;
-			strText = (String) displayContacts.getText();
-			strText += "\n" +"updateGroupAPI : " + "\n" + result;
-			Log.i("updateGroupAPI","OnSuccess : RESULT :  " + result);
-			
-			ShowGroups("TESTGROUP"); //grpname or null 
-			displayContacts.setText(strText);
-			return;
-		}
-
-		@Override
-		public void onError(InAppMessagingError error) {
-			Log.i("updateGroupAPI on error", "onError");
-
-		}
-	}
-	
-	private void ShowGroups(String grpName) {
-		aabManager = new AABManager(Config.fqdn,
-									authToken,
-									new displayGroupsListener());
-		aabManager.GetGroups(pageParams, grpName);
-		
-	}
-	
-	private class displayGroupsListener implements ATTIAMListener {
-
-		@Override
-		public void onSuccess(Object response) {
-			GroupResultSet groupResultSet = (GroupResultSet) response;
-			
-			if (null != groupResultSet) {
-				strText = (String) displayContacts.getText();
-				Group[] groups_arr = groupResultSet.getGroups();
-				for (int i=0; i < groups_arr.length; i++) {
-					Group grp = groups_arr[i];
-					strText += "\n" + grp.getGroupId() + ", " + 
-							grp.getGroupName() + ", " + grp.getGroupType();
-					
-				}
-			Log.i("displayGroups","OnSuccess : RESULT :  " + strText);
-			
-			displayContacts.setText(strText);
-			
-			return;
-			}
-		}
-
-		@Override
-		public void onError(InAppMessagingError error) {
-			Log.i("displayGroups on error", "onError");
-
-		}
 	}
 
 	@Override
