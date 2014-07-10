f7c9fd28e56a123dc0f5172f063ab52645404857
Ganesh Kumar, Dinesh Kumar (dg185p) <dg185p@us.att.com>
2014-07-09 19:32:22 -0700

    Updated AppScope for IAM SampleApp

--- IAM SampleApp/src/com/att/iamsampleapp/Config.java
+++ IAM SampleApp/src/com/att/iamsampleapp/Config.java
@@ -9,10 +9,10 @@ public class Config {
 	public static final int maxRecipients 			= 	10;
 	public static final int	maxAttachments			= 	21;
 	public static final String fqdn		 			= 	"https://api.att.com";
+	public static final String appScope	 			= 	 "IMMN,MIM";
 	
 	public static final String clientID				= 	 APP_KEY;
 	public static final String secretKey 			= 	 APP_SECRET;
-	public static final String appScope	 			= 	 APP_SCOPE;
 	public static final String redirectUri  		= 	 REDIRECT_URI;
 	public static final String iamDownloadDirectory = "InAppMessagingDownloads";
 }
