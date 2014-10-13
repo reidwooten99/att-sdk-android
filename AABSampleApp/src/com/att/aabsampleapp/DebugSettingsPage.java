d548bc6b8e2c0bb8857eba006ecc0118341a5859
Surender Mohan <smohan36@gmail.com>
2014-10-13 15:12:27 -0700

    update the debug settings ui

--- AABSampleApp/src/com/att/aabsampleapp/DebugSettingsPage.java
+++ AABSampleApp/src/com/att/aabsampleapp/DebugSettingsPage.java
@@ -6,37 +6,36 @@ import com.att.api.oauth.OAuthToken;
 import com.att.api.util.Preferences;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class DebugSettingsPage extends Activity {
 
-	private Button m_forceOffNetButton = null;
-	private Button m_suppressButton = null;
-	private Button m_clearCookiesButton = null;
+	private CheckBox m_forceOffNetCheckBox = null;
+	private CheckBox m_suppressCheckBox = null;
+	private CheckBox m_clearCookiesCheckBox = null;
 	private Button m_applyButton = null;
-	private Button m_forceCheckBox = null;
+	private CheckBox m_forceACExpiresCheckBox = null;
 	private EditText m_curAC = null;
 	private EditText m_refreshToken = null;
 	private TextView m_curACTime = null;
-	private boolean OFF_NET = false;
-	private boolean SUPPRESS = false;
-	private boolean CLEAR_COOKIES = false;
 	
 	protected void InitializeStateFromPreferences() {
 
 		m_curAC = (EditText)findViewById(R.id.curAC);
 		m_refreshToken = (EditText)findViewById(R.id.refreshToken);
 		m_curACTime = (TextView)findViewById(R.id.curACTime);
-		m_forceOffNetButton = (Button) findViewById(R.id.forceOffNetCheckBox);
-		m_suppressButton = (Button) findViewById(R.id.forceSuppressCheckBox);		
+		m_forceOffNetCheckBox = (CheckBox) findViewById(R.id.forceOffNetCheckBox);
+		m_suppressCheckBox = (CheckBox) findViewById(R.id.forceSuppressCheckBox);		
+		m_clearCookiesCheckBox = (CheckBox) findViewById(R.id.clearCookiesCheckBox);		
+		m_forceACExpiresCheckBox = (CheckBox) findViewById(R.id.forceACExpiresCheckBox);
 
 		Preferences prefs = new Preferences(getApplicationContext());		
 		if (prefs != null) {
@@ -46,15 +45,8 @@ public class DebugSettingsPage extends Activity {
 					AabManager.GetReduceTokenExpiryInSeconds_Debug()));
 
 			String savedCustomParam = prefs.getString(Config.customParamSettingName, "");
-			if (savedCustomParam.contains("bypass_onnetwork_auth")) {
-				m_forceOffNetButton.setBackgroundResource(R.drawable.check_mark);
-				OFF_NET = true;
-			}
-
-			if (savedCustomParam.contains("suppress_landing_page")) {
-				m_suppressButton.setBackgroundResource(R.drawable.check_mark);
-				SUPPRESS = true;
-			}
+			m_forceOffNetCheckBox.setChecked(savedCustomParam.contains("bypass_onnetwork_auth"));
+			m_suppressCheckBox.setChecked(savedCustomParam.contains("suppress_landing_page"));
 		}		
 	}
 
@@ -65,54 +57,13 @@ public class DebugSettingsPage extends Activity {
 		
 		InitializeStateFromPreferences();
 
-		m_forceOffNetButton.setOnClickListener(new Button.OnClickListener(){
-			public void onClick(View v) {    	 		 
-				if (!OFF_NET){
-					OFF_NET = true;
-					m_forceOffNetButton.setBackgroundResource(R.drawable.check_mark);
-				}
-				else{
-					OFF_NET = false;
-					m_forceOffNetButton.setBackgroundColor(Color.LTGRAY);
-				}
-			}
-		});
-
-		m_suppressButton.setOnClickListener(new Button.OnClickListener(){
-			public void onClick(View v) {   	 
-				if (!SUPPRESS){
-					m_suppressButton.setBackgroundResource(R.drawable.check_mark);
-					SUPPRESS = true;
-				}
-				else {
-					m_suppressButton.setBackgroundColor(Color.LTGRAY);
-					SUPPRESS = false;
-				}
-			}
-		});
-
-		m_clearCookiesButton = (Button) findViewById(R.id.clearCookiesCheckBox);
-		m_clearCookiesButton.setOnClickListener(new Button.OnClickListener(){
+		m_forceACExpiresCheckBox.setOnClickListener(new CheckBox.OnClickListener(){
 			public void onClick(View v) {
-				if (!CLEAR_COOKIES){
-					m_clearCookiesButton.setBackgroundResource(R.drawable.check_mark);  
-					CLEAR_COOKIES = true;
-				}
-				else {
-					m_clearCookiesButton.setBackgroundColor(Color.LTGRAY);
-					CLEAR_COOKIES = false;
-				}
-			}	 
-		});
-
-		m_forceCheckBox = (Button) findViewById(R.id.forceCheckBox);
-		m_forceCheckBox.setOnClickListener(new Button.OnClickListener(){
-			public void onClick(View v) {
-				m_forceCheckBox.setBackgroundResource(R.drawable.check_mark);
-				m_curACTime.setText("0");
+				// set the expiry time to current Unix Epoch time
+				m_curACTime.setText(String.format("%d", (new Date().getTime())/1000));
 			}
 		});
-
+		
 		m_applyButton = (Button) findViewById(R.id.applyButton);
 		m_applyButton.setOnClickListener(new Button.OnClickListener(){
 			public void onClick(View v) {
@@ -120,7 +71,7 @@ public class DebugSettingsPage extends Activity {
 				OAuthToken token = null;
 
 				Preferences prefs = new Preferences(getApplicationContext());		
-				if (CLEAR_COOKIES){
+				if (m_clearCookiesCheckBox.isChecked()){
 					prefs.setString(Config.accessTokenSettingName, "");
 					prefs.setString(Config.refreshTokenSettingName, "");
 					prefs.setLong(Config.tokenExpirySettingName, 0);
@@ -131,10 +82,10 @@ public class DebugSettingsPage extends Activity {
 					cookieManager.removeSessionCookie(); 
 				}
 				else {
-					if (OFF_NET) {
+					if (m_forceOffNetCheckBox.isChecked()) {
 						customParamValue = "bypass_onnetwork_auth";
 					} 
-					if (SUPPRESS) {
+					if (m_suppressCheckBox.isChecked()) {
 						customParamValue = customParamValue.isEmpty() ? "suppress_landing_page" : customParamValue + "," + "suppress_landing_page";
 					}
 					prefs.setString(Config.customParamSettingName, customParamValue);
