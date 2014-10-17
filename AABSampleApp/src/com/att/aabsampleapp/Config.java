27fb54e53de25c872fd9b9d280c3f01c1512a386
Surender Mohan <smohan36@gmail.com>
2014-10-17 00:11:30 -0700

    updates to config.java

--- AABSampleApp/src/com/att/aabsampleapp/Config.java
+++ AABSampleApp/src/com/att/aabsampleapp/Config.java
@@ -7,14 +7,15 @@ public class Config {
 	// Enter the following details from the application created in
 	// http://developer.att.com
 	public static final String clientID = UPDATE_APP_KEY;
-	public static final String secretKey = UPDATE_APP_SECRET;
-	public static final String redirectUri = UPDATE_REDIRECT_URI;
+	public static final String secretKey = UPDATE_APP_KEY;
+	public static final String redirectUri = UPDATE_APP_KEY;
 	// Testers can test the customParam functionality by setting this config value to following combinations
 	// "" - default behavior - do not send any custom_param
 	// "bypass_onnetwork_auth"
 	// "suppress_landing_page"
 	// "bypass_onnetwork_auth,suppress_landing_page"
 	public static final String customParam = "";
-	// reduceTokenExpiryInSeconds_Debug is parameter that is used to test access token expiration logic
-	public static final long reduceTokenExpiryInSeconds_Debug = 0;
+	// lowerTokenExpiryTimeTo parameter can be used to developer to lower access token expiry time
+	// keep it as -1, if you do not want to override the expiration value returned by AT&T's API
+	public static final long lowerTokenExpiryTimeTo = -1;
 }
