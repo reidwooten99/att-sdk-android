574569976f09d8624ce60800be12756740504fb1
Surender Mohan <smohan36@gmail.com>
2014-11-06 21:49:53 -0800

    undo appKey/secret update

--- ADS/src/com/att/ads/sample/AdsViewActivity.java
+++ ADS/src/com/att/ads/sample/AdsViewActivity.java
@@ -98,8 +98,8 @@ public class AdsViewActivity extends Activity implements ATTAdViewListener {
 		} catch (Exception e) {
 			Log.e(TAG, "Eror :" + e.fillInStackTrace());
 		}*/
-		appKey = "7vcbpgntyzhiihvbe1gacdkdye47aw8a";
-		secret = "yye37yciyqckzxso9reud4ge9zf0yv4d";
+		appKey = UPDATE_APP_KEY_HERE;
+		secret = UPDATE_SECRET_KEY_HERE;
 		
 		// Category always in lower case
 		if(null == appKey || null == secret){
