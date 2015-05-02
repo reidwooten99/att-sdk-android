12fcf7a67125497ba9c9170a034fc66d0f6d5d18
Bruce Williams <brucew@isoftstone.com>
2015-05-02 17:22:04 -0400

    remove obsolete IAM bits

--- IAMSDK/src/com/att/api/immn/listener/AttSdkTokenUpdater.java
+++
@@ -1,8 +0,0 @@
-package com.att.api.immn.listener;
-
-import com.att.api.oauth.OAuthToken;
-
-public interface AttSdkTokenUpdater {	
-	public void onTokenUpdate(OAuthToken newToken);
-	public void onTokenDelete();
-}
--- IAMSDK/src/com/att/api/immn/service/ChangeType.java
+++
@@ -1,6 +0,0 @@
-package com.att.api.immn.service;
-
-public enum ChangeType {
-
-	NONE, ADD, DELETE, UPDATE;
-};
